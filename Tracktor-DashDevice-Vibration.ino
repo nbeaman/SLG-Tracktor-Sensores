05f244578e927471fc0ae24f1dfca828bc5ff511
nbeaman <nbeaman@fau.edu>
2021-02-02 08:45:56 -0500

    Mute button works better
    change LED 0 in showHoseConnectedAlertLEDs()  this function was called colorSwipe() previously.

--- Tracktor-DashDevice-Vibration.ino
+++ Tracktor-DashDevice-Vibration.ino
@@ -4,10 +4,11 @@
 
 SoftwareSerial XBee(7, 6);    // XIAO
 
-#define LED_PIN       5
-#define VIBRATION_PIN 2
-#define BUZZER_PIN    1
-#define BUTTON_PIN    9
+#define LED_PIN         5
+#define VIBRATION_PIN   2
+#define BUZZER_PIN      1
+#define BUTTON_PIN      9
+#define LED_BRIGHTNESS  10
 
 #define LED_COUNT     7
 
@@ -15,26 +16,34 @@ int VibrationThreshold  = 25;
 int VibrationValue      = 0;
 int MotionStatus        = 0;
 boolean ShowTankLevel   = true;
-boolean BuzzerOn        = true;
+boolean G_BuzzerOn        = true;
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 
-boolean HoseConnected = false;
+boolean G_HoseConnected = false;
 
 float TankWaterLevel        = 0;
 float TankWaterLevel_EMPTY  = 17.46;
 float TankWaterLevel_FULL   = 18.56;
 float LED_Status_Previouse  = 0;
 
+//=========================================
+int DBUG = 1;
+//=========================================
 
 void setup()
 {
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.setBrightness(LED_BRIGHTNESS); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.clear();
+  strip.show();
+  
   // Baud rate MUST match XBee settings (as set in XCTU)
 
   delay(3000);    // TEST TO SEE IF THIS HELPS UPLOADING FILES TO XIAO!!!!!!!!
-  Serial.begin(115200);
+  if(DBUG) Serial.begin(115200);
   
   delay(1000);
 
@@ -43,7 +52,7 @@ void setup()
   
   XBee.begin(9600);
 
-  strip.setBrightness(10); // Set BRIGHTNESS to about 1/5 (max = 255)
+
       
 }
 
@@ -52,94 +61,135 @@ void loop()
 {
 
   ShowTankLevel = checkButton_ShowTankLevel(ShowTankLevel);
- 
-  if (HoseConnected){
-      colorWipe(strip.Color(255,   0,   0), 50); // Red
-      strip.show();
 
-      BuzzerOn = checkButton_BuzzerOnOff(BuzzerOn);
+   // ====================[ Read incomming data on XBee ]===========
+   if (XBee.available()){
+     String XBeeIN      = getXBeePayload();
+     if(DBUG) Serial.println("Payload:" + XBeeIN);
+     String XBeeIN_TYPE = XBeeIN.substring( 0, 1 );
+     if(DBUG) Serial.println("Type:" + XBeeIN_TYPE);
+     String XBeeIN_DATA = XBeeIN.substring( 1, (XBeeIN.length()-1) );
+     if(DBUG) Serial.println("Data:" + XBeeIN_DATA);
+       
+     if (XBeeIN_TYPE == "H"){
+        if (XBeeIN_DATA.substring(0,1) == "H") G_HoseConnected = true;
+        if (XBeeIN_DATA.substring(0,1) == "L") G_HoseConnected = false;
+     }
+     if (XBeeIN_TYPE == "T") TankWaterLevel = XBeeIN_DATA.toFloat();
+   }
+   //===============================================================
+
+   ShowTankLevel = checkButton_ShowTankLevel(ShowTankLevel);
+    
+   if (G_HoseConnected){  
+      Serial.println("Hose Connected:" + String(G_HoseConnected));
+
+      G_BuzzerOn = checkButton_G_BuzzerOnOff(G_BuzzerOn);
+
+      showHoseConnectedAlertLEDs(strip.Color(255,   0,   0), 50, G_BuzzerOn); // Red, 50ms, buzzer sound on or off
       
       VibrationValue = analogRead(VIBRATION_PIN);
       
       if (VibrationValue > VibrationThreshold) {
-        MotionStatus = HIGH;
+        if(DBUG) Serial.println("Motion Detected");
+        if (G_BuzzerOn) BuzzerSound();
       } else {
-        MotionStatus = LOW;
+        delay(600);
       }
+                 
+      clearLEDs(G_BuzzerOn, ShowTankLevel, G_HoseConnected);
+   } else {
 
-      if (MotionStatus == HIGH){
-         Serial.println("Motion Detected");
-         if (BuzzerOn) BuzzerSound();
-      } else {
-         delay(600);
-       } 
-           
-      clearLEDs(BuzzerOn, ShowTankLevel, HoseConnected);
-      
-  } else {
-    
       if (ShowTankLevel) ShowTankWaterLevel(TankWaterLevel);
-  }
-  
-  if (XBee.available())  
-  { 
-    char c = XBee.read();
-
-    Serial.println(c);
-    
-    if (c == 'H') HoseConnected = true;
-    if (c == 'L') HoseConnected = false;
-    if (c == 'T') HoseConnected = false;
-  }
+   } 
 }
 
-boolean checkButton_BuzzerOnOff(boolean buzzer_on){
+String getXBeePayload(){
+
+  String strRETURN = "";
+  int i = 1;
+  char c;
+  boolean done = false;
+
+  while (!done){
+      if (XBee.available()){
+        c = XBee.read();
+        delay(30);
+        strRETURN = strRETURN + String(c);
+        if(DBUG) if (c == char(0x0D) ) Serial.println("Found CR");
+      }      
+      if ( (c == char(0x0D) ) or ( i > 25 )) done=true; 
+      i=i+1;      
+  }         
+
+  return strRETURN;
+  
+}
+
+boolean checkButton_G_BuzzerOnOff(boolean buzzer_on){
     
   boolean r;
   
   if (digitalRead(BUTTON_PIN) == LOW){    // if buzzer_on is true, then pushing the button will make it flase
+
+    LED_AcceptButtonPressed();      // 450ms delay
     
     if (buzzer_on){
-      strip.setPixelColor(0, strip.Color(255,   255,   0) );
       r = false;
     }else{
-      strip.setPixelColor(0, strip.Color(0,   0,   0) );
       r = true;
-    }
-    strip.show();   
-    Serial.println("BuzzerOn Button Pushed: " + String(r)); 
+    } 
+    if(DBUG) Serial.println("G_BuzzerOn Button Pushed: " + String(r)); 
   } else {
     r = buzzer_on;
   }
-  
-  delay(1000);
   return r;  
 }
 
 boolean checkButton_ShowTankLevel(boolean show_tank_level){
   
   boolean r;
-  Serial.println(digitalRead(BUTTON_PIN));
+
   if (digitalRead(BUTTON_PIN) == LOW){
     
+    strip.clear();
+    strip.show();
+    
     if (show_tank_level){     // if show_tank_level was true, pushing the button will turn off lights and set not to show tank level LEDS
-      strip.clear();
-      strip.show();
       r = false;
     }else{
       r = true;
     } 
       
-    Serial.println("ShowTankLevel Button Pushed:" + String(r)); 
-     
+    if(DBUG) Serial.println("ShowTankLevel Button Pushed:" + String(r)); 
+
+    strip.setPixelColor(0, strip.Color(0,   255,   0) );
+    strip.show();
+    delay(300);
+    LED_AcceptButtonPressed();      // 450ms delay
+    
   }else{
     r = show_tank_level;
   }
   
-  delay(1000); 
   return r;
 }
 
+void LED_AcceptButtonPressed(){
+    for (int k=75; k>0; k--){
+      strip.setPixelColor(0, strip.Color(0,   255,   0) );
+      strip.setPixelColor(2, strip.Color(0,   255,   0) );
+      strip.setPixelColor(3, strip.Color(0,   255,   0) );      
+      strip.setPixelColor(5, strip.Color(0,   255,   0) );
+      strip.setPixelColor(6, strip.Color(0,   255,   0) );
+      strip.setBrightness(k);
+      strip.show();
+      delay(6);
+    }
+    strip.setBrightness(LED_BRIGHTNESS);
+    strip.clear();
+    strip.show();  
+}
 void ShowTankWaterLevel(float wlvalue){
       
       // Code to show level based on wlvalue, if 0 show nothing
@@ -151,7 +201,13 @@ void ShowTankWaterLevel(float wlvalue){
       strip.setPixelColor(4, strip.Color(0,   0,   255) ); 
       strip.show(); 
 }
-void colorWipe(uint32_t color, int wait) {
+void showHoseConnectedAlertLEDs(uint32_t color, int wait, boolean buzzer_on) {
+  Serial.println("Showing Red Alert LEDs");
+  if (buzzer_on){
+      strip.setPixelColor(0, strip.Color(0,   0,   0) );
+  }else{
+      strip.setPixelColor(0, strip.Color(255,   255,   0) );
+  }
   for(int i=1; i<strip.numPixels(); i++) { // For each pixel in strip...
     strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
     strip.show();                          //  Update strip to match
@@ -185,7 +241,7 @@ float GetTankWaterLevel(){
   s = s + char(XBee.read());
   s = s + char(XBee.read());
   s = s + char(XBee.read());
-  Serial.println(TankWaterLevel);
+  if(DBUG) Serial.println(TankWaterLevel);
   return float(TankWaterLevel);
 }
 
