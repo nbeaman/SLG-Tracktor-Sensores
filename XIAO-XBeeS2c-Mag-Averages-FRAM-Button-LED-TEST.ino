2cfec7683774d19311531c85a761edba517e6716
nbeaman <nbeaman@fau.edu>
2021-02-10 17:04:41 -0500

    after cable splice to allow programming

--- XIAO-XBeeS2c-Mag-Averages-FRAM-Button-LED-TEST.ino
+++ XIAO-XBeeS2c-Mag-Averages-FRAM-Button-LED-TEST.ino
@@ -9,7 +9,7 @@ String DBUG = "FRAM";
 //---[ FRAM ]-------------------------------------
 // FRAM Variables
 #define EEPROM_ADDR         0x50  // the default address of the I2C device
-int TAPS_EEPROM_LOCATION =  0;
+int TAPS_EEPROM_RAM_START_LOCATION =  0;
 //------------------------------------------------
 
 
@@ -22,11 +22,15 @@ float   xt =  0;
 float   yt =  0;
 float   zt =  0;
 
-float   TAPS = 0;
-float   TAPSNORMALNEGATIVE = -6;
-float   TAPSNORMALPOSITIVE = 6;
-char    STATUS = 'L';
-String  MESSAGE;
+float   TAPS                  = 0;
+float   TAPS_PREVIOUSE_VAL    = 0;
+float   TAPS_AVERAGE          = 0;
+float   TAPS_EEPROM_VAL       = 0;
+float   TAPS_THRESHHOLD       = 6;
+float   TAPSNORMALNEGATIVE    = -6;
+float   TAPSNORMALPOSITIVE    = 6;
+char    STATUS                = 'L';
+String  MESSAGE               = "";
 
 
 // Tlv493d Opject
@@ -50,24 +54,33 @@ void setup() {
     if (DBUG == "FRAM") Serial.println("I2C EEPROM not identified ... check your connections?\r\n");
     while (1) delay(10);
   }
+
+  String temp = EEPROM_Read(TAPS_EEPROM_RAM_START_LOCATION);
+  TAPS_EEPROM_VAL = temp.toFloat();
+  //***
+  if (0 > TAPS_EEPROM_VAL) TAPS_EEPROM_VAL = TAPS_EEPROM_VAL * -1;      //make the value positive b/c we test for a threshhold postive and negetive (not knowing if magnet is up or down)
+  //***
+  Serial.println("TAPS_EEPROM_VAL:" + String(TAPS_EEPROM_VAL) );
   //------------------------------------------------
+
+  TAPS_THRESHHOLD = TAPS_THRESHHOLD + TAPS_EEPROM_VAL;        // example if threshhold is 6 and normal value in EEPROM is -3.55 b/c we already made the EEPROM val positive the value is 9.55
   
   Tlv493dMagnetic3DSensor.begin();
   XBee.begin(9600);
 
   pinMode(ledPin, OUTPUT);
-  pinMode(XBeeSleepPin, OUTPUT);
+  //pinMode(XBeeSleepPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   
 }
 
 void loop() {
 
-  digitalWrite(XBeeSleepPin, HIGH);  // XBee Sleep
+  //digitalWrite(XBeeSleepPin, HIGH);  // XBee Sleep
   
   digitalWrite(ledPin, HIGH);
 
-  //String ReadTest = EEPROM_Read(TAPS_EEPROM_LOCATION);
+  //String ReadTest = EEPROM_Read(TAPS_EEPROM_RAM_START_LOCATION);
   //if (DBUG == "FRAM") Serial.print("\r\nRead: *" + ReadTest + "*\r\n");
         
   for (int i=1; i <=samplenumber; i++){
@@ -75,7 +88,13 @@ void loop() {
    Tlv493dMagnetic3DSensor.updateData();
    delay(100);
 
-   if (digitalRead(buttonPin)) ButtonWasPushed();
+   if (digitalRead(buttonPin)){
+      if(TAPS_AVERAGE != 0){
+        ButtonWasPushed();
+        EEPROM_Write(TAPS_EEPROM_RAM_START_LOCATION, String(TAPS_AVERAGE)); 
+        Serial.println("PUSHED TAPS_AVERAGE=" + String(TAPS_AVERAGE));      
+      }
+   }
    delay(100);
 
    digitalWrite(ledPin, LOW);
@@ -84,19 +103,27 @@ void loop() {
    yt = yt + Tlv493dMagnetic3DSensor.getY();
    zt = zt + Tlv493dMagnetic3DSensor.getZ();
    delay(100);
-
-   //if (digitalRead(buttonPin)) ButtonWasPushed();
-   delay(100);
        
   }
-  
+  if( (TAPS !=0) and (TAPS_PREVIOUSE_VAL !=0) ){
+    TAPS_AVERAGE = (TAPS_AVERAGE + TAPS_PREVIOUSE_VAL + TAPS) /3;
+  }
+  TAPS_PREVIOUSE_VAL = TAPS;
   TAPS = xt + yt + zt;
 
-  digitalWrite(XBeeSleepPin, LOW);    // XBee Wake up
+  //digitalWrite(XBeeSleepPin, LOW);    // XBee Wake up
   delay(300);                         // *** Important to wait - destination XBee sees garbage otherwise
   
-  if ((TAPS > TAPSNORMALPOSITIVE) or (TAPS < TAPSNORMALNEGATIVE)){
-    STATUS = 'H';
+  //if ((TAPS > TAPSNORMALPOSITIVE) or (TAPS < TAPSNORMALNEGATIVE)){
+  //  STATUS = 'H';
+  //} else {
+  //  STATUS = 'L';    
+  //}
+
+  
+  Serial.print("+-" + String(TAPS_THRESHHOLD) + ",");
+  if ((TAPS > TAPS_THRESHHOLD) or (TAPS < (TAPS_THRESHHOLD * -1) )){        // we do not know if the magnet is upright or pointed down when hose is connected
+    STATUS = 'H';                                                           // so we test for positive pole and negative.
   } else {
     STATUS = 'L';    
   }
@@ -108,14 +135,12 @@ void loop() {
 
   MESSAGE = MESSAGE + String(TAPS);
   
-  Serial.println(MESSAGE);
+  Serial.println("MESSAGE:" + MESSAGE);
   for (int i=0; i < MESSAGE.length(); i++){
     XBee.write(MESSAGE[i]);
   }
   
   XBee.write(char(0x0d));
-
-  TAPS =0;
      
   xt=0;
   yt=0;
@@ -169,8 +194,8 @@ String EEPROM_Read(int location){
 
 void EEPROM_Write(int location, String S){
 
-  int sInx = 0;               // used to access each char of S (S[0...(SLen-1)])
-  char SLen = S.length();     // number of bytes in S
+  int   sInx = 0;               // used to access each char of passed in string S (S[0...(SLen-1)])
+  char  SLen = S.length();     // number of bytes in S
 
   // write the number of bytes in S to the starting location in memory
   i2ceeprom.write8(location,SLen);
